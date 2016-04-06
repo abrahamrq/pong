883f853e5bbb31120dd30a3b0d7b4e3f7b88883e
abrahamrq <abraham.rq03@gmail.com>
2016-04-06 11:33:25 -0500

    hey

--- pong_examen/pong_examen/main.cpp
+++ pong_examen/pong_examen/main.cpp
@@ -9,12 +9,14 @@
 #endif
 #include <string>
 #include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <sstream>
-#include "Sound.h"
+#include "imageloader.h"
+//#include "Sound.h"
 
 
-Sound die = Sound("/Users/taniagarridosalido/Dropbox/ITESM-ITC Decimo Semestre/Graficas/pong/pong_examen/pong_examen/sounds/die.wav");
+//Sound die = Sound("/Users/taniagarridosalido/Dropbox/ITESM-ITC Decimo Semestre/Graficas/pong/pong_examen/pong_examen/sounds/die.wav");
 
 ///////////////////////////////////CLASSES//////////////////////////////////////
 class Raquet{
@@ -153,7 +155,7 @@ class Ball{
     }
     
     void resetPosition(){
-      die.PlaySound();
+      //die.PlaySound();
       this->x_translate = 0;
       this->y_translate = 0;
       this->speed_x = .01;
@@ -217,6 +219,74 @@ class Ball{
 bool paused = false, started = false;
 Player players[2];
 Ball ball;
+std::string fullPath = __FILE__;
+const int TEXTURE_COUNT=1;
+static GLuint texName[TEXTURE_COUNT];
+
+////////////////////////////////////////////////////////////////////////////////
+
+void handleKeypress(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27: //Escape key
+            exit(0);
+    }
+}
+
+//le borramos el exceso para solo obtener el Path padre
+void getParentPath()
+{
+    for (int i = (int)fullPath.length()-1; i>=0 && fullPath[i] != '/'; i--) {
+        fullPath.erase(i,1);
+    }
+}
+//Makes the image into a texture, and returns the id of the texture
+void loadTexture(Image* image,int k)
+{
+    
+    glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    //Filtros de ampliacion y redución con cálculo mas cercano no es tan bueno pero es rápido
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    
+    //Filtros de ampliacion y redución con cálculo lineal es mejo pero son más calculos
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
+                 0,                            //0 for now
+                 GL_RGB,                       //Format OpenGL uses for image
+                 image->width, image->height,  //Width and height
+                 0,                            //The border of the image
+                 GL_RGB, //GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
+                 //as unsigned numbers
+                 image->pixels);               //The actual pixel data
+}
+
+
+
+void initRendering()
+{
+    //Declaración del objeto Image
+    Image* image;
+    GLuint i=0;
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    glGenTextures(TEXTURE_COUNT, texName); //Make room for our texture
+    char  ruta[200];
+    sprintf(ruta,"%s%s", fullPath.c_str() , "texturas/burger.bmp");
+    image = loadBMP(ruta);loadTexture(image,i++);
+   
+    delete image;
+}
 
 void generateBall(){
   ball = Ball();
@@ -268,7 +338,7 @@ bool collide(float ball_x, float ball_y, float player_x, float player_y, int pla
   // std::cout << "ball: " << ball_x_left << ", " << ball_x_right << ", " << ball_y_down << ", " << ball_y_top << "player: " << player_x_left << ", " << player_x_right << ", " << player_y_down << ", " << player_y_top << std::endl;
   bool col = ((ball_y_top >= player_y_down && ball_y_down <= player_y_top) && (ball_x_right >= player_x_left && ball_x_left <= player_x_right));
   if (col){
-    ping.PlaySound();
+    // ping.PlaySound();
   }
   return col;
 }
@@ -398,17 +468,235 @@ void init(){
   generateBall();
 }
 
+void timer(int value)
+{
+    glutPostRedisplay();
+    // glutTimerFunc(1000,timer,0);
+}
+
 int main(int argc, char** argv){
   glutInit(&argc,argv);
-  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInitWindowSize(500,500);
   glutInitWindowPosition(0,0);
   glutCreateWindow("Pong A01195653 - A01138941");
+  getParentPath();
+  initRendering();
   glutReshapeFunc(reshape);
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboardActions);
   glutSpecialFunc(specialActions);
   init();
-  //glutTimerFunc(1000, myTimer,   1);
+  glutTimerFunc(1000,timer,0);
   glutMainLoop();
 }
+
+
+////////////////////////////IMAGE///////////////////////////////////////////////
+
+#include <assert.h>
+#include <fstream>
+
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h)
+{
+    
+}
+
+Image::~Image()
+{
+    delete[] pixels;
+}
+
+namespace
+{
+    //Converts a four-character array to an integer, using little-endian form
+    int toInt(const char* bytes)
+    {
+        return (int)(((unsigned char)bytes[3] << 24) |
+                     ((unsigned char)bytes[2] << 16) |
+                     ((unsigned char)bytes[1] << 8) |
+                     (unsigned char)bytes[0]);
+    }
+    
+    //Converts a two-character array to a short, using little-endian form
+    short toShort(const char* bytes)
+    {
+        return (short)(((unsigned char)bytes[1] << 8) |
+                       (unsigned char)bytes[0]);
+    }
+    
+    //Reads the next four bytes as an integer, using little-endian form
+    int readInt(ifstream &input)
+    {
+        char buffer[4];
+        input.read(buffer, 4);
+        return toInt(buffer);
+    }
+    
+    //Reads the next two bytes as a short, using little-endian form
+    short readShort(ifstream &input)
+    {
+        char buffer[2];
+        input.read(buffer, 2);
+        return toShort(buffer);
+    }
+    
+    //Just like auto_ptr, but for arrays
+    template<class T>
+    class auto_array
+    {
+    private:
+        T* array;
+        mutable bool isReleased;
+    public:
+        explicit auto_array(T* array_ = NULL) :
+        array(array_), isReleased(false)
+        {
+        }
+        
+        auto_array(const auto_array<T> &aarray)
+        {
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+        
+        ~auto_array()
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+        }
+        
+        T* get() const
+        {
+            return array;
+        }
+        
+        T &operator*() const
+        {
+            return *array;
+        }
+        
+        void operator=(const auto_array<T> &aarray)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+        
+        T* operator->() const
+        {
+            return array;
+        }
+        
+        T* release()
+        {
+            isReleased = true;
+            return array;
+        }
+        
+        void reset(T* array_ = NULL)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = array_;
+        }
+        
+        T* operator+(int i)
+        {
+            return array + i;
+        }
+        
+        T &operator[](int i)
+        {
+            return array[i];
+        }
+    };
+}
+
+Image* loadBMP(const char* filename)
+{
+    ifstream input;
+    input.open(filename, ifstream::binary);
+    assert(!input.fail() || !"Could not find file");
+    char buffer[2];
+    input.read(buffer, 2);
+    assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
+    input.ignore(8);
+    int dataOffset = readInt(input);
+    
+    //Read the header
+    int headerSize = readInt(input);
+    int width;
+    int height;
+    switch (headerSize)
+    {
+        case 40:
+            //V3
+            width = readInt(input);
+            height = readInt(input);
+            input.ignore(2);
+            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+            assert(readShort(input) == 0 || !"Image is compressed");
+            break;
+        case 12:
+            //OS/2 V1
+            width = readShort(input);
+            height = readShort(input);
+            input.ignore(2);
+            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+            break;
+        case 64:
+            //OS/2 V2
+            assert(!"Can't load OS/2 V2 bitmaps");
+            break;
+        case 108:
+            //Windows V4
+            assert(!"Can't load Windows V4 bitmaps");
+            break;
+        case 124:
+            //Windows V5
+            assert(!"Can't load Windows V5 bitmaps");
+            break;
+        default:
+            assert(!"Unknown bitmap format");
+    }
+    
+    //Read the data
+    int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+    int size = bytesPerRow * height;
+    auto_array<char> pixels(new char[size]);
+    input.seekg(dataOffset, ios_base::beg);
+    input.read(pixels.get(), size);
+    
+    //Get the data into the right format
+    auto_array<char> pixels2(new char[width * height * 3]);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                pixels2[3 * (width * y + x) + c] =
+                pixels[bytesPerRow * y + 3 * x + (2 - c)];
+            }
+        }
+    }
+    
+    input.close();
+    return new Image(pixels2.release(), width, height);
+}
+
+////////////////////////////////////////////////////////////////////////////////
